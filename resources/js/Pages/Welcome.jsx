484b47df89f9f2698d3658227f174fa4e3cdcd23
Alim Mughanil Karim <alim.mughanil23@gmail.com>
2023-05-13 10:39:34 +0700

    init project with laravel react with breeze

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,41 @@
+import { Link, Head } from "@inertiajs/react";
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="relative min-h-screen bg-gray-100 bg-center sm:flex sm:justify-center sm:items-center bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+                <div className="p-6 text-right sm:fixed sm:top-0 sm:right-0">
+                    {auth.user ? (
+                        <Link
+                            href={route("dashboard")}
+                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route("login")}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route("register")}
+                                className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+
+                <div className="p-6 mx-auto max-w-7xl lg:p-8">
+                    <p>Selamat datang</p>
+                </div>
+            </div>
+        </>
+    );
+}
